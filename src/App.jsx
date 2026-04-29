0c4dae96e347af591963abe71f6733972a430184
Nagato_Dolbaeb <amx50100tank@gmail.com>
2026-04-29 13:13:28 +0300

    перешли на css модули

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import styles from `./App.module.css`;
+
 export default function App() {
   return (
     <>
       <h1 className="header">MiniStore</h1>
-      <p>Первый экран</p>
+      
+      <main>
+        <section className= "Atagoboder">
+          <h1>Товар 1</h1>
+          <p>Описание</p>
+          <span>24/09</span>
+        </section>
+        
+        <section className= "Atagoboder">
+          <h1>Товар 2</h1>
+          <p>Описание</p>
+          <span>25/09</span>
+        </section>
+
+        <section className= "Atagoboder">
+          <h1>Товар 3</h1>
+          <p>Описание</p>
+          <span>04/09</span>
+        </section>
+      </main>
     </>
+    
   );
 }
