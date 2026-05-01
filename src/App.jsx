beb54a71d995c1b1e686705550d45c4ff66ec531
Nagato_Dolbaeb <amx50100tank@gmail.com>
2026-05-01 13:31:14 +0300

    Шапка сайта скелет

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ export default function App() {
   return (
     <>
       <>
-         <div className="Header">
+        
+          <div id = "wrap">
+          <header>
+            <div className="title">
+            <h1> <img width = "150"src = "../IMG/Icon_Lucker.png"></img>Lucker Store</h1>
+            </div>
+            <ul className = "menu">
+              <button><li><a href = "#">Контакты</a></li></button>
+              <button><li><a href = "#">О нас</a></li></button>
+              <button><li><a href = "#">Услуги</a></li></button>
+            </ul>
+          </header>
+          </div>
           
-          <h1>Atago Shop</h1>
-         </div>
-
-
-       <div className="catolog">
+         <body>
+         <div className="catolog">
         <h2>Каталог Товаров</h2>
        <div className="container">
         <div className="row">
@@ -197,8 +206,40 @@ export default function App() {
         </div>
 
 
+        <div className="row">
+          <div className="col-md-4">
+            <div className="product">
+              <div className="image">
+                <img width = "400" src = "../IMG/Ellis_Tshort.png"></img>
+              </div>
+
+              <div className="info">
+                <h3>Футболка Эллиса из Left 4 Dead 2</h3>
+                 <ul className="rating">
+                  <li>⭐</li>
+                  <li>⭐</li>
+                  <li>⭐</li>
+                  <li>⭐</li>
+                  <li>⭐</li>
+                 </ul>
+                <div>
+                  <span className="price">917<small>₽</small></span>
+                  <button className="add-to-cart">Купить</button>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
+
       </div>
        </div>
+         </body>
+
+
+      
       </>
     </>
     
